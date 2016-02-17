0c51c6119ceef534dd609ee31ac6d3d7325dbf61
Andrew McLeod <admcleod@gmail.com>
2016-02-17 12:42:52 +0100

    import correct lib

--- lib/charms/spark.py
+++ lib/charms/spark.py
@@ -1,7 +1,7 @@
 from glob import glob
 from path import Path
 from subprocess import CalledProcessError
-from charms.spark import Livy
+from charms.livy import Livy
 import jujuresources
 from charmhelpers.core import hookenv
 from charmhelpers.core import host
